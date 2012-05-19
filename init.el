7a1958ccb36117e7f109ea198a0b1022603c333f
Jeff Weiss <jweiss@redhat.com>
2012-05-19 17:19:20 -0400

    generify

--- init.el
+++ init.el
@@ -7,7 +7,7 @@
   (package-refresh-contents))
 
 ;; Add in your own as you wish:
-(defvar my-packages '(starter-kit starter-kit-lisp starter-kit-bindings clojure-mode durendal ac-slime yasnippet)
+(defvar my-packages '(starter-kit starter-kit-lisp starter-kit-bindings clojure-mode durendal ac-slime)
   "A list of packages to ensure are installed at launch.")
 
 (dolist (p my-packages)
@@ -29,31 +29,4 @@
 
 (add-hook 'slime-repl-mode-hook (lambda () (paredit-mode 1)))
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(erc-autojoin-channels-alist (quote (("freenode.net" "#emacs" "#clojure" "#katello" "#pulp") ("devel.redhat.com" "#cloud-qe" "#systemengine" "#systemengine-qe" "#candlepin"))))
- '(erc-enable-logging t)
- '(erc-generate-log-file-name-function (lambda (buffer target nick server port) (let ((file (concat (if target (concat target "@")) server ":" (cond ((stringp port) port) ((numberp port) (number-to-string port))) ".txt"))) (convert-standard-filename file))))
- '(erc-join-buffer (quote bury))
- '(erc-log-channels-directory "~/.erc/logs/")
- '(erc-log-mode t)
- '(erc-log-write-after-insert t)
- '(erc-log-write-after-send t)
- '(erc-save-buffer-on-part nil)
- '(erc-save-queries-on-quit nil)
- '(ibuffer-saved-filter-groups (quote (("normal" ("Clojure" (mode . clojure-mode)) ("irc" (mode . erc-mode)) ("git" (mode . magit-mode)) ("org" (mode . org-mode)) ("emacs" (or (name . "^\\*scratch\\*$") (name . "^\\*Messages\\*$")))))))
- '(ibuffer-saved-filters (quote (("gnus" ((or (mode . message-mode) (mode . mail-mode) (mode . gnus-group-mode) (mode . gnus-summary-mode) (mode . gnus-article-mode)))) ("programming" ((or (mode . emacs-lisp-mode) (mode . cperl-mode) (mode . c-mode) (mode . java-mode) (mode . idl-mode) (mode . lisp-mode)))))))
- '(org-agenda-files (quote ("~/tasks/7212467cf49c6e11eaff/jweiss.org"))))
-
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :background "black" :foreground "white" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 90 :width normal :foundry "unknown" :family "DejaVu Sans Mono"))))
- '(erc-input-face ((t (:foreground "gray"))))
- '(magit-item-highlight ((t (:background "gray10")))))
 
--- jweiss.el
+++ jweiss.el
@@ -1,6 +1,6 @@
 ;;time loading of this file
-(require 'cl) ; a rare necessary use of REQUIRE
-(defvar *emacs-load-start* (current-time))
+;(require 'cl) ; a rare necessary use of REQUIRE
+
 
 ;;disable suspending emacs on ctrl-z
 (global-set-key (kbd "C-z") 'undo)
@@ -8,25 +8,20 @@
 ;jump to line
 (global-set-key (kbd "C-c M-l") 'goto-line)
 ;buffer switch 
-(global-set-key (kbd "C-,") 'ido-switch-buffer)
+;(global-set-key (kbd "C-,") 'ido-switch-buffer)
 ;;frame-switch
-(global-set-key (kbd "C-q") 'windmove-up)
-(global-set-key (kbd "C-z") 'windmove-down)
+;(global-set-key (kbd "C-q") 'windmove-up)
+;(global-set-key (kbd "C-z") 'windmove-down)
 ;;use ibuffer
 (global-set-key (kbd "C-x C-b") 'ibuffer)
 
 ;;use w tiling window mgr
-(setq pop-up-frames nil)
+;(setq pop-up-frames nil)
 
 ;;associate some file extensions with modes
-(add-to-list 'auto-mode-alist '("\\.*repo$" . conf-unix-mode))
+;(add-to-list 'auto-mode-alist '("\\.*repo$" . conf-unix-mode))
 
 ;;basic colors
-(custom-set-faces
- '(default ((t (:inherit nil :stipple nil :background "black" :foreground "white" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 90 :width normal :foundry "unknown" :family "DejaVu Sans Mono")))))
-(global-hl-line-mode 1)
-(set-face-background 'hl-line "#151500")
-
 (desktop-save-mode 1)
 
 ;;auto-complete, ac-slime
@@ -38,13 +33,6 @@
 (add-to-list 'ac-modes 'slime-repl-mode)
 (add-hook 'slime-mode-hook 'set-up-slime-ac)
 (add-hook 'slime-repl-mode-hook 'set-up-slime-ac)
-
-;;org-mode
-(add-to-list 'auto-mode-alist '("\\.org\\'" . org-mode))
-(add-hook 'org-mode-hook 'turn-on-font-lock) ; not needed when global-font-lock-mode is on
-(global-set-key "\C-cl" 'org-store-link)
-(global-set-key "\C-ca" 'org-agenda)
-(global-set-key "\C-cb" 'org-iswitchb)
 
 ;; More syntax coloring
 (defun tweak-clojure-syntax (mode)
@@ -84,39 +72,11 @@
 (defcljface clojure-special      "#0074e8"   "Clojure special")
 (defcljface clojure-double-quote "#00920A"   "Clojure special")
 
-;;clojure colors
-(defun set-clojure-colors (mode-fn)
-  (tweak-clojure-syntax mode-fn)
-  (set-face-foreground 'font-lock-string-face "#ffddaa")
-  (set-face-foreground 'clojure-parens "gray27")
-  (set-face-attribute 'clojure-parens nil :underline nil :bold t)
-  (set-face-foreground 'clojure-brackets "#445f5f")
-  (set-face-attribute 'clojure-brackets nil :underline nil :bold t)
-  (set-face-foreground 'clojure-double-quote "LightSalmon")
-  (set-face-foreground 'clojure-braces "#666644")
-  (set-face-attribute 'clojure-braces nil :underline nil :bold t)
-  (set-face-foreground 'clojure-keyword "#ffcccc")
-  (set-face-attribute 'clojure-keyword nil :underline nil :bold nil :italic t)
-  (set-face-foreground 'font-lock-keyword-face "#eeaaff")
-  (set-face-attribute 'font-lock-keyword-face nil :underline nil :bold t :italic nil)
-  (set-face-foreground 'clojure-java-call "#ccffcc")
-  (set-face-foreground 'clojure-namespace "#0000ff")
-  (set-face-attribute 'clojure-namespace nil :underline nil :bold t)
 
-  (set-face-attribute 'font-lock-function-name-face nil :underline nil :bold t)
-  (set-face-foreground 'font-lock-function-name-face "#ffeeaa")
-  (set-face-attribute 'font-lock-builtin-face nil :underline nil :bold t)
-  (set-face-foreground 'font-lock-builtin-face "#aaddff")
-
-  (set-face-foreground 'clojure-special "#bbbbff")
-  (set-face-attribute 'clojure-special nil :underline nil :bold t :italic t))
 
 (autoload 'clojure-mode "clojure-mode")
-(set-clojure-colors 'clojure-mode)
-(set-clojure-colors 'slime-repl-mode)
-
-(add-hook 'eshell-mode-hook 
-          (lambda()(paredit-mode 1)))
+;(set-clojure-colors 'clojure-mode)
+;(set-clojure-colors 'slime-repl-mode)
 
 ;(add-hook 'clojure-mode-hook 'durendal-enable-auto-compile)
 (add-hook 'slime-repl-mode-hook 'durendal-slime-repl-paredit)
@@ -124,19 +84,11 @@
 (add-hook 'sldb-mode-hook 'durendal-dim-sldb-font-lock)
 ;(add-hook 'slime-compilation-finished-hook 'durendal-hide-successful-compile)
 
-(autoload 'yas/initialize "yasnippet")
-(yas/initialize)
-(setq yas/root-directory '("~/.emacs.d/snippets"
-                           "~/.emacs.d/elpa/yasnippet-0.6.1/snippets"))
-(mapc 'yas/load-directory yas/root-directory)
-
-
-(add-hook 'clojure-mode-hook 'yas/minor-mode-on)
 (add-hook 'clojure-mode-hook 
           (lambda () 
             (define-key clojure-mode-map (kbd "M-[") 'paredit-wrap-square)
             (define-key clojure-mode-map (kbd "M-{") 'paredit-wrap-curly)))
-(eval-after-load 'clojure-mode (yas/reload-all))
+;(eval-after-load 'clojure-mode (yas/reload-all))
 
 (autoload 'paredit-wrap-square "paredit")
 (add-hook 'slime-connected-hook
@@ -162,192 +114,4 @@
 
 ;;browse link shortcut
 (global-set-key (kbd "C-c M-b") 'browse-url-at-point)
-
-;;ERC
-(setq nick-face-list '())
-(setq erc-nick-uniquifier "_")
-
-;; Define the list of colors to use when coloring IRC nicks.                                                                     
-(setq-default erc-colors-list '("pink" "light green" "yellow"
-                                "gray" "thistle" "tan" "violet"
-                                "mint cream" "white" "gold"
-                                "dark khaki" "orange" "plum"
-                                "light blue" "pale green"
-                                "#ffeeaa" "#aaddff" "#bbeebb"))
-
-(defun build-nick-face-list ()
-  "build-nick-face-list builds a list of new faces using the
-foreground colors specified in erc-colors-list. The nick faces
-created here will be used to format IRC nicks."
-  (setq i -1)
-  (setq nick-face-list
-        (mapcar
-         (lambda (COLOR)
-           (setq i (1+ i))
-           (list (custom-declare-face
-                  (make-symbol (format "erc-nick-face-%d" i))
-                  (list (list t (list :foreground COLOR)))
-                  (format "Nick face %d" i))))
-         erc-colors-list)))
-
-(defun my-insert-modify-hook ()
-  "This insert-modify hook looks for nicks in new messages and
-computes md5(nick) and uses substring(md5_value, 0, 4)
-mod (length nick-face-list) to index the face list and produce
-the same face for a given nick each time it is seen. We get a lot
-of collisions this way, unfortunately, but it's better than some
-other methods I tried. Additionally, if you change the order or
-size of the erc-colors-list, you'll change the colors used for
-nicks."
-  (if (null nick-face-list) (build-nick-face-list))
-  (save-excursion
-    (goto-char (point-min))
-    (if (looking-at "<\\([^>]*\\)>")
-        (let ((nick (match-string 1)))
-          (put-text-property (match-beginning 1) (match-end 1)
-                             'face (nth
-                                    (mod (string-to-number
-                                          (substring (md5 nick) 0 4) 16)
-                                         (length nick-face-list))
-                                    nick-face-list))))))
-
-;; This adds the ERC message insert hook.                                                                                        
-(add-hook 'erc-insert-modify-hook 'my-insert-modify-hook)
-
-(add-hook 'erc-after-connect
-	  '(lambda (SERVER NICK)
-	     (cond
-	      ((string-match "freenode\\.net" SERVER)
-	       (erc-message "PRIVMSG" "NickServ identify 111111jm"))
-	      
-	      ((string-match "irc\\.devel\\.redhat" SERVER)
-	       (erc-message "PRIVMSG" "userserv login jweiss 111111jm")))))
-
-(defun irc-jweiss ()
-  (interactive)
-  (erc :server "irc.freenode.net" :port 6667 :nick "jweiss")
-  (erc :server "irc.devel.redhat.com" :port 6667 :nick "jweiss"))
-
-;;channel tracking
- (setq erc-track-exclude-types '("JOIN" "NICK" "PART" "QUIT" "MODE"
-                                 "324" "329" "332" "333" "353" "477"))
-
-
-(defun reset-erc-track-mode ()
-  (interactive)
-  (setq erc-modified-channels-alist nil)
-  (erc-modified-channels-update))
-
-(global-set-key (kbd "C-c r") 'reset-erc-track-mode)
-
-(defadvice erc-track-find-face (around erc-track-find-face-promote-query activate)
-  (if (erc-query-buffer-p) 
-      (setq ad-return-value (intern "erc-current-nick-face"))
-    ad-do-it))
-
-;;turn off auto-fill-mode which apparently breaks long lines into
-;;multiple messages
-(add-hook 'erc-mode-hook (lambda () (auto-fill-mode 0)))
-
-;;notify
-;;; Notify me when a keyword is matched (someone wants to reach me)
-(autoload 'notifications-notify "notifications")
-(setq erc-keywords '("jweiss"))
-(erc-match-mode 1)
-
-(defvar my-erc-page-nick-alist nil
-  "Alist of nicks and the last time they tried to trigger a
-notification")
-
-(defvar my-erc-page-timeout 10
-  "Number of seconds that must elapse between notifications from
-the same person.")
-
-(defvar notification-sound-file "/usr/share/sounds/freedesktop/stereo/complete.oga")
-
-(defun my-erc-page-allowed (nick &optional delay)
-  "Return non-nil if a notification should be made for NICK.
-If DELAY is specified, it will be the minimum time in seconds
-that can occur between two notifications.  The default is
-`my-erc-page-timeout'."
-  (unless delay (setq delay my-erc-page-timeout))
-  (let ((cur-time (time-to-seconds (current-time)))
-        (cur-assoc (assoc nick my-erc-page-nick-alist))
-        (last-time))
-    (if cur-assoc
-        (progn
-          (setq last-time (cdr cur-assoc))
-          (setcdr cur-assoc cur-time)
-          (> (abs (- cur-time last-time)) delay))
-      (push (cons nick cur-time) my-erc-page-nick-alist)
-      t)))
-
-(defun my-erc-notify (nick channel message)
-  (start-process "notif" nil "play"
-                 "-q" notification-sound-file)
-  (condition-case ()
-      (notifications-notify :title (format "%s in %s" nick channel)
-                            ;; Remove duplicate spaces
-                            :body (replace-regexp-in-string " +" " " message)
-                            :sound-file notification-sound-file)
-    (error nil)))
-
-(defun my-erc-page-me (match-type nick msg)
-  "Notify the current user when someone sends a message that
-matches a regexp in `erc-keywords'."
-  (interactive)
-  ;(message (format "page called: %s %s %s" match-type nick msg))
-  (when (and (eq match-type 'keyword)
-             ;; I don't want to see anything from the erc server
-             (null (string-match "\\`\\([sS]erver\\|localhost\\)" nick))
-             ;; or bots
-             (null (string-match "\\(bot\\|serv\\)!" nick))
-             ;; or from those who abuse the system
-             (my-erc-page-allowed nick))
-    (my-erc-notify (car (split-string nick "!"))
-                   (or (erc-default-target) "#unknown") msg)))
-(add-hook 'erc-text-matched-hook 'my-erc-page-me)
-
-(defun my-erc-page-me-PRIVMSG (proc parsed)
-  (let ((nick (car (erc-parse-user (erc-response.sender parsed))))
-        (target (car (erc-response.command-args parsed)))
-        (msg (erc-response.contents parsed)))
-    (when (and (erc-current-nick-p target)
-               (not (erc-is-message-ctcp-and-not-action-p msg))
-               (my-erc-page-allowed nick))
-      (my-erc-notify nick nick msg)
-      nil)))
-(add-hook 'erc-server-PRIVMSG-functions 'my-erc-page-me-PRIVMSG)
-
-(defun my-notify-erc (match-type nickuserhost message)
-  "Notify when a message is received."
-  (notifications-notify :title (format "%s in %s"
-                                       ;; Username of sender
-                                       (car (split-string nickuserhost "!"))
-                                       ;; Channel
-                                       (or (erc-default-target) "#unknown"))
-                        ;; Remove duplicate spaces
-                        :body (replace-regexp-in-string " +" " " message)
-                        :sound-file notification-sound-file))
-
-
-;;syntax highlight RoR log files
-(require 'generic-x) 
-
-(define-generic-mode 
-    'ror-log-mode ;; name of the mode to create
-  '("!!")         ;; comments start with '!!'
-  '("ERROR" "WARN" 
-    "DEBUG" "INFO")                        ;; some keywords
-  '(("^\\[.*\\]" . font-lock-comment-face) 
-    ("[0-9]\\{4\\}-[0-9]\\{2\\}-[0-9]\\{2\\} \\([0-9]\\{2\\}:\\)+[0-9]\\{2\\}" . 'font-lock-type-face)
-    ("/.*:in `.*'$" . 'font-lock-warning-face)
-    ("/.*:[0-9]+$" . 'font-lock-warning-face)) ;; ';' is a a built-in 
-  '("production.log")        ;; files for which to activate this mode 
-  nil                        ;; other functions to call
-  "A mode for RoR log files" ;; doc string for this mode
-  )
-
-(message "My .emacs loaded in %ds" (destructuring-bind (hi lo ms) (current-time)
-                           (- (+ hi lo) (+ (first *emacs-load-start*) (second *emacs-load-start*)))))
 
