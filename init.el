e65d115b9350491efa12cf381b14bcea8c215752
Jeff Weiss <jweiss@redhat.com>
2012-06-10 22:46:42 -0400

    add mark-multiple

--- init.el
+++ init.el
@@ -1,6 +1,8 @@
 (require 'package)
 (add-to-list 'package-archives
              '("marmalade" . "http://marmalade-repo.org/packages/") t)
+(add-to-list 'package-archives
+             '("melpa" . "http://melpa.milkbox.net/packages/") t)
 (package-initialize)
 
 (when (not package-archive-contents)
