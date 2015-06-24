f2dd9fffd43fbff355cdcb989e62efe31d5a8847
Jeff Weiss <me@jweiss.com>
2015-06-24 05:39:24 -0400

    Java mode setup

--- jweiss-java.el
+++ jweiss-java.el
@@ -1,1 +1,8 @@
+(require 'eclim)
+(require 'eclimd)
+(require 'cc-mode)
 (define-key eclim-mode-map (kbd "M-.") 'eclim-java-find-declaration)
+(define-key java-mode-map (kbd "<down>") 'next-line)
+(add-hook 'java-mode-hook 'smartparens-strict-mode)
+(add-hook 'java-mode-hook 'eclim-mode)
+(define-key java-mode-map (kbd "RET") 'my-electric-return)
