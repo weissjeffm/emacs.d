5460549c694e48afc4b27c6144cc7a2f7fbabbbc
Jeff Weiss <jweiss@lgscout.com>
2017-05-31 13:48:57 -0400

    Bind key to take both additions in conflict resolution

--- jweiss.el
+++ jweiss.el
@@ -94,6 +94,14 @@
      (set-face-foreground 'diff-added "green4")
      (set-face-foreground 'diff-removed "red3")))
 
+;; option to combine both a and b diffs
+(defun ediff-copy-both-to-C ()
+  (interactive)
+  (ediff-copy-diff ediff-current-difference nil 'C nil
+                   (concat
+                    (ediff-get-region-contents ediff-current-difference 'A ediff-control-buffer)
+                    (ediff-get-region-contents ediff-current-difference 'B ediff-control-buffer))))
+(add-hook 'ediff-keymap-setup-hook (lambda () (define-key ediff-mode-map "B" 'ediff-copy-both-to-C)))
 ;; y instead of yes
 (defalias 'yes-or-no-p 'y-or-n-p)
 
